0868de757baa126f92b99f1d5e6b37985bed758a
cindrella-19 <pragathiani123@gmail.com>
2021-01-07 18:17:46 +0530

    I added track

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    track=new Ground(500,200,30,200);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -61,4 +61,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    track.display();
 }
